defe43effafc4f9388a838f010446ac6d049dcb9
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-05 19:29:29 +0100

    Two Sum

---
+++ solutions/leetcode.py
@@ -0,0 +1,8 @@
+def test(result, correct):
+    print(
+        ("\x1b[92m" if result == correct else "\x1b[91m")
+        + str(result)
+        + " = "
+        + str(correct)
+        + "\x1b[0m",
+    )
---
+++ solutions/two_sum.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/two-sum/description/
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        for first_index, num in enumerate(nums):
+            try:
+                return [first_index, nums.index(target - num, first_index + 1)]
+            except ValueError:
+                pass
+
+
+from leetcode import *
+
+test(Solution().twoSum([2, 7, 11, 15], 9), [0, 1])
+test(Solution().twoSum([3, 2, 4], 6), [1, 2])
+test(Solution().twoSum([3, 3], 6), [0, 1])
