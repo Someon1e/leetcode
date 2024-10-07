7d8dd169824875e09b4fd33aa6d5679cd9480557
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-07 20:22:07 +0100

    Minimum String Length After Removing Substrings

---
+++ solutions/minimum_string_length_after_removing_substrings.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/minimum-string-length-after-removing-substrings/description/
+
+
+class Solution:
+    def minLength(self, s: str) -> int:
+        while True:
+            old = s
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
+            if old == s:
+                return len(s)
+
+
+from leetcode import *
+
+test(Solution().minLength("ABFCACDB"), 2)
+test(Solution().minLength("ACBBD"), 5)
