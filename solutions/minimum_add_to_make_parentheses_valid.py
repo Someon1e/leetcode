6c39848727f7fa09958ee66461810062a89dd8e8
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-09 22:00:52 +0100

    Minimum Add to Make Parentheses Valid

---
+++ solutions/minimum_add_to_make_parentheses_valid.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
+
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        needed = 0
+        for character in s:
+            match character:
+                case "(":
+                    stack.append(character)
+                case ")":
+                    if stack:
+                        assert stack.pop() == "("
+                    else:
+                        needed += 1
+                case _:
+                    raise Exception()
+        return len(stack) + needed
+
+
+from leetcode import *
+
+test(Solution().minAddToMakeValid("())"), 1)
+test(Solution().minAddToMakeValid("((("), 3)
