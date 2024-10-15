31c09dcefa055f65b2166d0768e115a5202f4509
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-15 21:09:30 +0100

    Separate Black and White Balls

---
+++ solutions/separate_black_and_white_balls.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/separate-black-and-white-balls/description/
+
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        steps = 0
+        counter = 0
+        for character in s:
+            if character == "1":
+                counter += 1
+            elif character == "0":
+                steps += counter
+        return steps
+
+
+from leetcode import *
+
+test(Solution().minimumSteps("101"), 1)
+test(Solution().minimumSteps("100"), 2)
+test(Solution().minimumSteps("0111"), 0)
