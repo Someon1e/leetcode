18acd68e4b9caa219bfe3bc1dd4e235d02756c56
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-19 20:51:20 +0100

    Single Number

---
+++ solutions/single_number.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/single-number/description
+
+class Solution:
+    def singleNumber(self, nums) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
+
+from leetcode import *
+
+
+test(Solution().singleNumber([2, 2, 1]), 1)
+test(Solution().singleNumber([4, 1, 2, 1, 2]), 4)
+test(Solution().singleNumber([1]), 1)
