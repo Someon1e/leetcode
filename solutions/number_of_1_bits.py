b4335a42b6bc9781c489cf19a086db49725bae0b
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-20 18:03:20 +0100

    Number of 1 Bits

---
+++ solutions/number_of_1_bits.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/number-of-1-bits/description/
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return n.bit_count()
+
+
+from leetcode import *
+
+test(Solution().hammingWeight(11), 3)
+test(Solution().hammingWeight(128), 1)
+test(Solution().hammingWeight(2147483645), 30)
