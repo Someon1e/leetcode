4b9de7d5db0d60821e07b6aabae599702feb9425
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-21 19:26:19 +0100

    Longest Common Prefix

---
+++ solutions/a.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/longest-common-prefix/description/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        index = 0
+        while True:
+            if len(strs[0]) <= index:
+                return strs[0][:index]
+            first = strs[0][index]
+            for string in strs[1:]:
+                if len(string) <= index or string[index] != first:
+                    return string[:index]
+            index += 1
+
+
+from leetcode import *
+
+test(Solution().longestCommonPrefix(["flower", "flow", "flight"]), "fl")
+test(Solution().longestCommonPrefix(["dog", "racecar", "car"]), "")
