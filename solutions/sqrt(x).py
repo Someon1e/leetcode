544092508f1aa9e744b98ee26ddca5d3dde15574
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-23 21:13:03 +0100

    Sqrt(x)

---
+++ solutions/sqrt(x).py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/sqrtx/description/
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        lower = 1
+        upper = x
+        while lower <= upper:
+            middle = (lower + upper) // 2
+            squared = middle * middle
+            if squared == x:
+                return middle
+            elif squared > x:
+                # decrease upperbound
+                upper = middle - 1
+            else:
+                # increase lowerbound
+                lower = middle + 1
+        return upper
+
+
+from leetcode import *
+
+test(Solution().mySqrt(4), 2)
+test(Solution().mySqrt(8), 2)
