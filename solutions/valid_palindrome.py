b4a58c7646ec454a905b7b6557f0fcf1149dae1a
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-24 21:13:55 +0100

    Valid Palindrome

---
+++ solutions/valid_palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        processed = "".join(character for character in s if character.isalnum()).lower()
+        return processed[::-1] == processed
+
+
+from leetcode import test
+
+test(Solution().isPalindrome("A man, a plan, a canal: Panama"), True)
+test(Solution().isPalindrome("race a car"), False)
+test(Solution().isPalindrome(" "), True)
