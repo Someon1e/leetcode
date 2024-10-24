d8e99747c45b5a929b1f48efd98dc7fc9e91edd3
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-24 21:09:11 +0100

    Remove Element

---
+++ solutions/remove_element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums, val: int) -> int:
+        count = 0
+        for num in nums:
+            if num != val:
+                nums[count] = num
+                count += 1
+        return count
+
+
+# TODO: tests
