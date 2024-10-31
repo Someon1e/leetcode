6c52e7f484a585900efb3bac6d108b557d269949
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-10-31 21:04:11 +0000

    Missing Number

---
+++ solutions/missing_number.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/missing-number/description/
+
+
+class Solution:
+    def missingNumber(self, nums) -> int:
+        result = 0
+        for x in range(len(nums)):
+            result ^= x + 1
+            result ^= nums[x]
+        return result
+
+
+from leetcode import *
+
+test(Solution().missingNumber([3, 0, 1]), 2)
+test(Solution().missingNumber([0, 1]), 2)
+test(Solution().missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]), 8)
