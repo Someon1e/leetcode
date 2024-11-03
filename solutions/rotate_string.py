3300aafff1e429c56794cfbc8740b20c4d3f0870
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-03 20:34:35 +0000

    Rotate String

---
+++ solutions/rotate_string.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/rotate-string/description/
+
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for shift in range(len(s)):
+            if s[shift:] + s[:shift] == goal:
+                return True
+        return False
+
+
+from leetcode import *
+
+test(Solution().rotateString("abcde", "cdeab"), True)
+test(Solution().rotateString("abcde", "abced"), False)
