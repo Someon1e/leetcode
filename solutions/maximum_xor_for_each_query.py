be8d50d44499f7912a5b5e004d7ea717ac381b28
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-08 21:52:18 +0000

    Maximum XOR for Each Query

---
+++ solutions/maximum_xor_for_each_query.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/maximum-xor-for-each-query/description/
+
+
+class Solution:
+    def getMaximumXor(self, nums, maximumBit: int):
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        answers = []
+        bit_mask = (1 << maximumBit) - 1
+        for num in reversed(nums):
+            answers.append(xor ^ bit_mask)
+            xor ^= num
+        return answers
+
+
+from leetcode import *
+
+test(Solution().getMaximumXor([0, 1, 1, 3], 2), [0, 3, 2, 3])
+test(Solution().getMaximumXor([2, 3, 4, 7], 3), [5, 2, 6, 5])
+test(Solution().getMaximumXor([0, 1, 2, 2, 5, 7], 3), [4, 3, 6, 4, 6, 7])
