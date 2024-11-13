53f170199a15088c666fb7b5ec4683ae2c91f11f
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-13 22:40:09 +0000

    Happy Number

---
+++ solutions/happy_number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/happy-number/description/
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set([n])
+        while True:
+            sum_of_squares = 0
+            for x in str(n):
+                sum_of_squares += int(x) ** 2
+            n = sum_of_squares
+            if n == 1:
+                return True
+            elif n in seen:
+                return False
+            seen.add(n)
