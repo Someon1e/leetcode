e90e5e4c5cceb693dbb989ed9ec980da9d63390a
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-18 22:01:48 +0000

    Defuse the Bomb

---
+++ solutions/defuse_the_bomb.py
@@ -0,0 +1,13 @@
+class Solution:
+    def decrypt(self, code, k: int):
+        if k > 0:
+            repeated = code * 2
+            for i in range(len(code)):
+                code[i] = sum(repeated[i + 1 : i + 1 + k])
+        elif k < 0:
+            repeated = code * 2
+            for i in range(len(code)):
+                code[i] = sum(repeated[i + len(code) + k : i + len(code)])
+        else:
+            return [0] * len(code)
+        return code
