463e4084f7b88a10e287a07b5fe311870edd812a
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-20 20:59:19 +0000

    Rank Transform of an Array

---
+++ solutions/rank_transform_of_an_array.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/rank-transform-of-an-array/description/
+
+
+class Solution:
+    def arrayRankTransform(self, arr):
+        ranks = list(set(arr))
+        ranks.sort()
+        lookup = {value: rank + 1 for rank, value in enumerate(ranks)}
+        return [lookup[value] for value in arr]
+
+
+from leetcode import *
+
+test(Solution().arrayRankTransform([40, 10, 20, 30]), [4, 1, 2, 3])
+test(Solution().arrayRankTransform([100, 100, 100]), [1, 1, 1])
+test(
+    Solution().arrayRankTransform([37, 12, 28, 9, 100, 56, 80, 5, 12]),
+    [5, 3, 4, 2, 8, 6, 7, 1, 3],
+)
