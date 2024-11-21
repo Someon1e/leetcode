0328aff8c722ae4fe0f1511d30149c97af3f219d
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-21 21:26:39 +0000

    Count the Number of Consistent Strings

---
+++ solutions/count_the_number_of_consistent_strings.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words) -> int:
+        count = 0
+        for word in words:
+            consistent = 1
+            for character in word:
+                if character not in allowed:
+                    consistent = 0
+                    break
+            count += consistent
+        return count
+
+
+from leetcode import *
+
+test(Solution().countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]), 2)
+test(
+    Solution().countConsistentStrings("abc", ["a", "b", "c", "ab", "ac", "bc", "abc"]),
+    7,
+)
+test(
+    Solution().countConsistentStrings(
+        "cad", ["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"]
+    ),
+    4,
+)
