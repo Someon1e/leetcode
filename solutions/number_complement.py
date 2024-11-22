e072d09190e2a1769c7a4787a07bac604918571f
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-22 21:52:59 +0000

    Number Complement

---
+++ solutions/number_complement.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/number-complement/description/
+
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask = (1 << num.bit_length()) - 1
+        return ~num & mask
+
+
+from leetcode import *
+
+test(Solution().findComplement(5), 2)
+test(Solution().findComplement(1), 0)
