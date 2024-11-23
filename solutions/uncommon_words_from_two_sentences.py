5b089e60fbdba1b473b60d9f1d22eca0feccb08f
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-23 21:23:30 +0000

    Uncommon Words from Two Sentences

---
+++ solutions/uncommon_words_from_two_sentences.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/uncommon-words-from-two-sentences/description/
+
+from collections import Counter
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str):
+        return [a for a, b in Counter(s1.split(" ") + s2.split(" ")).items() if b == 1]
+
+
+from leetcode import *
+
+test(
+    Solution().uncommonFromSentences("this apple is sweet", "this apple is sour"),
+    ["sweet", "sour"],
+)
+test(
+    Solution().uncommonFromSentences("apple apple", "banana"),
+    ["banana"],
+)
