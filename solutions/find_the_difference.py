bbe10dd6874fab0289757543f86796b79876e318
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-29 21:00:05 +0000

    Find the Difference

---
+++ solutions/find_the_difference.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-the-difference/description/
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+
+        for character in t:
+            result ^= ord(character)
+        for character in s:
+            result ^= ord(character)
+
+        return chr(result)
+
+
+from leetcode import *
+
+test(Solution().findTheDifference("abcd", "abcde"), "e")
+test(Solution().findTheDifference("", "y"), "y")
