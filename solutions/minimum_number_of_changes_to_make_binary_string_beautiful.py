d7bc6c1c313319b0c371ea338a35d728336185f6
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-11-30 21:14:00 +0000

    Update minimum_number_of_changes_to_make_binary_string_beautiful.py

--- solutions/minimum_number_of_changes_to_make_binary_string_beautiful.py
+++ solutions/minimum_number_of_changes_to_make_binary_string_beautiful.py
@@ -9,4 +9,8 @@ class Solution:
                 changes += 1
         return changes
 
-# TODO: tests
+from leetcode import *
+
+test(Solution().minChanges("1001"), 2)
+test(Solution().minChanges("10"), 1)
+test(Solution().minChanges("0000"), 0)
