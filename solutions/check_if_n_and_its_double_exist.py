32e11a3ab75b119720ca390bfebbf273060508ac
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-12-01 20:58:52 +0000

    Check If N and Its Double Exist

---
+++ solutions/check_if_n_and_its_double_exist.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+
+
+class Solution:
+    def checkIfExist(self, arr) -> bool:
+        if arr.count(0) > 1:
+            return True
+        for x in arr:
+            if x != 0 and x * 2 in arr:
+                return True
+        return False
+
+
+from leetcode import *
+
+test(Solution().checkIfExist([10, 2, 5, 3]), True)
+test(Solution().checkIfExist([3, 1, 7, 11]), False)
