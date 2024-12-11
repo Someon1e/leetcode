1599da526fd561481e9a28a5157405a2c8a7f31e
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-12-11 21:45:08 +0000

    Valid Perfect Square

---
+++ solutions/valid_perfect_square.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/valid-perfect-square/description/
+
+lookup = set(n * n for n in range(46341))
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        return num in lookup
+
+
+from leetcode import *
+
+test(Solution().isPerfectSquare(16), True)
+test(Solution().isPerfectSquare(14), False)
