917d4ad3bca7d4392c5bae087a59ff764ed002a3
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-12-19 21:20:59 +0000

    Take Gifts From the Richest Pile

---
+++ solutions/take_gifts_from_the_richest_pile.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/take-gifts-from-the-richest-pile/description/
+
+
+from math import sqrt, floor
+
+
+class Solution:
+    def pickGifts(self, gifts, k: int) -> int:
+        for _ in range(k):
+            most = max(gifts)
+            index = gifts.index(most)
+            gifts[index] = floor(sqrt(most))
+        return sum(gifts)
+
+
+from leetcode import *
+
+test(Solution().pickGifts([25, 64, 9, 4, 100], 4), 29)
+test(Solution().pickGifts([1, 1, 1, 1], 4), 4)
